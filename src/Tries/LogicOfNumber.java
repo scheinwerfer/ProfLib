a6805bb8e29f9ee28fa1ee65611d744e515811ab
Vladislav Skachinsky <vskachinskyr@gmail.com>
2021-07-26 22:34:18 +0300

    new tries

--- src/Tries/LogicOfNumber.java
+++ src/Tries/LogicOfNumber.java
@@ -27,14 +27,19 @@ public class LogicOfNumber extends JPanel implements ActionListener {
 
     private void logic() {
         List<Character> number = new ArrayList<>();
+        //Запись символов в список
         for (int i = 0; i < txt.getText().length(); i++) {
             number.add(i, txt.getText().charAt(i));
         }
+        //Добавление пустого элемента, чтобы rotate нормально сдвигал элементы вправо
         number.add(' ');
         if (number.get(0) != '+') {
+            //Сдвиг элементов вправо на 1 позицию
             Collections.rotate(number, 1);
             number.set(0, '+');
         }
+        //Запись элементов в строку, потому что String.valueOf(number) выдает хуету
+        //[3,7,5]
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < number.size(); i++) {
             sb.append(number.get(i));
